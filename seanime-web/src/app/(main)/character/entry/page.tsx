3962efa1e6d911d04da4f8b83231456237f0537d
Tenru Yuko・天瑠ユコ <IsshouPrism@gmail.com>
2025-08-04 15:19:06 -0700

    Return To Monke

--- seanime-web/src/app/(main)/character/entry/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { CharacterEntryPage } from '@/app/(main)/character/_containers/character-entry-page'
-
-export default function Page() {
-    return <CharacterEntryPage />
-}
