1e77885c78fbd90cfffe1be94a2e36faa3ede4b9
Tenru Yuko・天瑠ユコ <IsshouPrism@gmail.com>
2025-08-11 20:42:51 -0700

    rem..

--- seanime-web/src/app/(main)/en-masse-downloader/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { EnMasseDownloader } from "@/app/(main)/_features/manga/en-masse-downloader"
-
-export default function EnMasseDownloaderPage() {
-    return <EnMasseDownloader />
-}
